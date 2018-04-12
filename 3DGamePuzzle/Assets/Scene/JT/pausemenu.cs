4e5838152d664475a49127c46b1891c7d4d9dbee
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-04-12 10:19:02 -0700

    Stuff and Changes -JT

---
+++ 3DGamePuzzle/Assets/Scene/JT/pausemenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausemenu : MonoBehaviour {
+    bool paused = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
+        {
+            paused = true;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        {
+            paused = false;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if (paused)
+        {
+            Time.timeScale = 0;
+            GetComponent<Canvas>().enabled = true;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if (paused == false)
+        {
+            Time.timeScale = 1;
+            GetComponent<Canvas>().enabled = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+    public void Resume()
+    {
+        GetComponent<Canvas>().enabled = false;
+        paused = false;
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
