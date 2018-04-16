1491bc5b644d37e5e71bab680b6f31f90b83d27a
Alexander Petty <18.alexander.petty@ksd.org>
2018-04-16 10:25:14 -0700

    MAZZZZZZZZZZZZE -ap

---
+++ 3DGamePuzzle/Assets/Scripts/Maze.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour {
+
+    public GameObject Player;
+    //public GameObject Go;
+    public Vector3 startPos;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Maze") {
+
+            transform.position = new Vector3(100, -100, 100);
+            Debug.Log("fnhdbgfuhcbedwgfy");
+
+        }
+
+    }
+}
