7c836fcb261b475bc40146b26f974f5d15149e8e
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-04-16 10:26:19 -0700

    Added my dev scene along with my raycastingscripts -JT

---
+++ 3DGamePuzzle/Assets/Scene/JT/CarRayCast.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class CarRayCast : MonoBehaviour {
+    public GameObject ObjectiveText;
+    // Use this for initialization
+    public Transform player;
+    NavMeshAgent agent;
+    void Start () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        //layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            ObjectiveText.GetComponent<Text>().text = ("Objective: This car looks like its in good condition. I wonder if there is keys to it anywhere.");
+            agent.destination = player.position;
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
