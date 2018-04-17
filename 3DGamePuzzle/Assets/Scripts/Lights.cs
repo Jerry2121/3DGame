3f6835009e587d024af7658b6ed20c063e2cbc30
Alexander Petty <18.alexander.petty@ksd.org>
2018-04-17 08:33:45 -0700

    let there be no light

--- 3DGamePuzzle/Assets/Scripts/Lights.cs
+++ 3DGamePuzzle/Assets/Scripts/Lights.cs
@@ -6,6 +6,8 @@ public class Lights : MonoBehaviour {
     public GameObject Spotlight;
     public bool StartOn;
     public bool On;
+    private bool enter = false;
+
     // Use this for initialization
     void Start()
     {
@@ -24,15 +26,39 @@ public class Lights : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) && On == false)
+        if (Input.GetKeyDown(KeyCode.R) && On == false && enter == true)
         {
             Spotlight.GetComponent<Light>().enabled = true;
             On = true;
         }
-        else if (Input.GetKeyDown(KeyCode.R) && On == true)
+        else if (Input.GetKeyDown(KeyCode.R) && On == true && enter == true)
         {
             Spotlight.GetComponent<Light>().enabled = false;
             On = false;
+        }
+    }
+    void OnGUI()
+    {
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 150, 30), "Press 'E' to open the door");
+        }
+    }
+    //Activate the Main function when player is near the door
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+
+    //Deactivate the Main function when player is go away from door
+    void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            enter = false;
         }
     }
 }
