d23e3bea81bd0846b33c8ba3a016be38fd0d150b
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-04-23 10:26:43 -0700

    Puzzle one complete -JT

--- 3DGamePuzzle/Assets/Scene/JT/Puzzles.cs
+++ 3DGamePuzzle/Assets/Scene/JT/Puzzles.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puzzles : MonoBehaviour {
     public float timer;
+    public Slider TimerSlide;
+    public GameObject SlideCanvas;
+    private bool complete;
     private bool enter = false;
-
+    public bool CarKeys;
     // Use this for initialization
     void Start () {
-		
+        SlideCanvas.GetComponent<Canvas>().enabled = false;
+        complete = false;
+
 	}
 
     void OnGUI()
@@ -21,25 +27,54 @@ public class Puzzles : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-
-	}
-    private void OnTriggerStay(Collider other)
+       // TimerSlide.value = timer;
+    }
+    public void OnTriggerStay(Collider other)
     {
 
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.E) && complete == false && CarKeys == false)
         {
             enter = false;
-
+            SlideCanvas.GetComponent<Canvas>().enabled = true;
             timer += Time.deltaTime;
+            float percent = timer / 5.0f;
+            percent *= 100;
+            int foo = Mathf.RoundToInt(percent);
+            TimerSlide.value = foo;
             if (timer >= 5)
             {
                 Debug.Log("Nothing Was Found.");
                 timer = 0;
+                complete = true;
             }
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.E) && CarKeys == false || complete == true && CarKeys == false)
         {
             timer = 0;
+            SlideCanvas.GetComponent<Canvas>().enabled = false;
+        }
+
+        if (Input.GetKey(KeyCode.E) && complete == false && CarKeys == true)
+        {
+            enter = false;
+            SlideCanvas.GetComponent<Canvas>().enabled = true;
+            timer += Time.deltaTime;
+            float percent = timer / 5.0f;
+            percent *= 100;
+            int foo = Mathf.RoundToInt(percent);
+            TimerSlide.value = foo;
+            if (timer >= 5)
+            {
+                Debug.Log("CarKeys Was found");
+                timer = 0;
+                complete = true;
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.E) && CarKeys == true || complete == true && CarKeys == true)
+        {
+
+            timer = 0;
+            SlideCanvas.GetComponent<Canvas>().enabled = false;
         }
     }
 }
---
+++ 3DGamePuzzle/Assets/Scene/JT/RandomCarKeys.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCarKeys : MonoBehaviour {
+    public GameObject[] Locations;
+    // Use this for initialization
+    void Start () {
+        int RandomKeyLocation = Random.Range(0, 10);
+        Locations[RandomKeyLocation].GetComponent<Puzzles>().CarKeys = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
