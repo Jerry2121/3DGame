df01cf62f73177dd5015cfe38036c9f6f95de98d
Alexander Petty <18.alexander.petty@ksd.org>
2018-04-30 09:23:43 -0700

    puzzles

--- 3DGamePuzzle/Assets/Scripts/TestThingy.cs
+++ 3DGamePuzzle/Assets/Scripts/TestThingy.cs
@@ -21,11 +21,23 @@ public class TestThingy : MonoBehaviour {
         {
             Puzzle3Complete = true;
         }
+        if (PlayerPrefs.GetInt("Puzzle1complete") == 0)
+        {
+            Puzzle1Complete = false;
+        }
+        if (PlayerPrefs.GetInt("Puzzle2complete") == 0)
+        {
+            Puzzle2Complete = false;
+        }
+        if (PlayerPrefs.GetInt("Puzzle3complete") == 0)
+        {
+            Puzzle3Complete = false;
+        }
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Puzzle1Complete)
+        if (Puzzle1Complete)
         {
             PlayerPrefs.SetInt("Puzzle1complete", 1);
         }
@@ -37,12 +49,40 @@ public class TestThingy : MonoBehaviour {
         {
             PlayerPrefs.SetInt("Puzzle3complete", 1);
         }
+        if(PlayerPrefs.GetInt("Puzzle1complete") == 1)
+        {
+            Puzzle1Complete = true;
+        }
+        if (PlayerPrefs.GetInt("Puzzle2complete") == 1)
+        {
+            Puzzle2Complete = true;
+        }
+        if (PlayerPrefs.GetInt("Puzzle3complete") == 1)
+        {
+            Puzzle3Complete = true;
+        }
+        if (PlayerPrefs.GetInt("Puzzle1complete") == 0)
+        {
+            Puzzle1Complete = false;
+        }
+        if (PlayerPrefs.GetInt("Puzzle2complete") == 0)
+        {
+            Puzzle2Complete = false;
+        }
+        if (PlayerPrefs.GetInt("Puzzle3complete") == 0)
+        {
+            Puzzle3Complete = false;
+        }
         if (Reset)
         {
             PlayerPrefs.SetInt("Puzzle1complete", 0);
             PlayerPrefs.SetInt("Puzzle2complete", 0);
             PlayerPrefs.SetInt("Puzzle3complete", 0);
             PlayerPrefs.SetInt("Health", 3);
+            Puzzle1Complete = false;
+            Puzzle2Complete = false;
+            Puzzle3Complete = false;
+            Reset = false;
         }
     }
 }
